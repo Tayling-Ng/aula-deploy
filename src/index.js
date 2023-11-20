5db3a5af2a8fcba6b2a1d2932dd0fe05acdc177d
unknown <taymng@yahoo.com.br>
2023-11-19 23:42:32 -0300

    Conexão com o banco de Dados

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 require('dotenv').config();
 
 const express = require('express');
+const knex = require('./conexao');
 
 const app = express();
+
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    return res.json('Deploy OK!');
-});
+    try {
+        const carros = await knex('carros');
+        return res.json(carros);
+    } catch (error) {
+        return res.status(500).json({ messagem: 'Erro do servidor!' });
+    }
 
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
-
 });
